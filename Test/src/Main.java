5732e2bc477074d5d0020f140e8548777287bda7
Chinh <cqnguyen21st@yahoo.com>
2016-11-03 19:35:22 -0700

    Hilber

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -10,16 +10,11 @@ import java.util.Set;
 public class Main {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		What b = new What();
-		Who a = new Who();
-		System.out.println(a.myName());
-=======
-		int[] a = {2};
-		int[] b = {2};
-		
-		System.out.println(Arrays.equals(a, b));
->>>>>>> 3f10010a026309560af89c203664e478bbefaa54
+		final int[] a = new int[2];
+		a[1] = 2;
+		System.out.println(a[1]);
+		a[1] = 3;
+		System.out.println(a[1]);
 	}
 	
 	public static class Combine {
--- UVA/src/Classy.java
+++ UVA/src/Classy.java
@@ -1,61 +1,117 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Classy {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException{
 		Scanner sc = new Scanner(System.in);
-		
-		int num = sc.nextInt();
-		sc.nextLine();
-		Person[] list = new Person[num];
-		int index = 0;
-		while (index < num) {
-			String line = sc.nextLine();
-			String[] level = line.split(" ");
-			list[index] = new Person(level);
-			
-			index++;
+		StringBuilder sb = new StringBuilder();
+		while (sc.hasNext()) {
+			int num = sc.nextInt();
+			sc.nextLine();
+			Person[] list = new Person[num];
+			int index = 0;
+			while (index < num) {
+				String line = sc.nextLine();
+				String[] level = line.split(" ");
+				list[index] = new Person(level);
+
+				index++;
+			}
+			Arrays.sort(list);
+
+			for (Person person : list) {
+				sb.append(person.name);
+				sb.append("\n");
+			}
 		}
-		Arrays.sort(list);
-	//	Arrays.sort(list, Collections.reverseOrder());
-		
-		for (Person person : list) {
-			System.out.println(person.name);
-		}
+		System.out.print(sb.toString());
+//		String string = sb.toString();
+//		System.out.print(string.substring(0, string.length() - 1));
 	}
 	
 	public static class Person implements Comparable<Person>{
 		String name;
-		int level;
+		String level;
+		int count = 0;
 		
 		public Person(String[] line) {
 			name = line[0].substring(0, line[0].length() - 1);
-			level = 0;
+			level = "";
+			StringBuilder sb = new StringBuilder();
 			for (int i = 1; i < line.length - 1; i++) {
-				level *= 10;
-				int num = line[i].equals("upper") ? 3 : line[i].equals("middle") ? 2 : 1;			
-				level += num;
+//				level *= 10;
+//				int num = line[i].equals("upper") ? 3 : line[i].equals("middle") ? 2 : 1;			
+//				level += num;
+				count++;
+				char c = line[i].equals("upper") ? '3' : line[i].equals("middle") ? '2' : '1';
+				sb.append(c);
 			}
+			level = sb.toString();
 		}
 
 		@Override
 		public int compareTo(Person other) {
 			// TODO Auto-generated method stub
 			int compare = 0;
-			int temp1 = this.level;
-			int temp2 = other.level;
-			while (temp1 > 0 && temp2 > 0 && compare == 0) {
-				if (temp1 % 10 > temp2 % 10) 
-					compare = -1;
-				else if (temp1 % 10 < temp2 % 10)
-					compare = 1;
-				else {
-					temp1 /= 10;
-					temp2 /= 10;
-				}
-				
+			String stemp1 = this.level;
+			String stemp2 = other.level;
+			int tempCount1 = this.count;
+			int tempCount2 = other.count;
+			StringBuilder sb = new StringBuilder();
+			while (tempCount1 < tempCount2) {
+				sb.append('2');
+				tempCount1++;
 			}
+			stemp1 = sb.toString() + stemp1;
+			
+			sb = new StringBuilder();
+			while (tempCount2 < tempCount1) {
+				sb.append('2');
+				tempCount2++;
+			}
+			stemp2 = sb.toString() + stemp2;
+			
+			for (int i = stemp1.length() - 1; i >= 0 && compare == 0; i--) {
+				if (stemp1.charAt(i) > stemp2.charAt(i)) 
+					compare = -1;
+				else if (stemp1.charAt(i) < stemp2.charAt(i)) 
+					compare = 1;
+	
+			}
+			
+			
+//			while (temp1 > 0 && temp2 > 0 && compare == 0) {
+//				if (temp1 % 10 > temp2 % 10) 
+//					compare = -1;
+//				else if (temp1 % 10 < temp2 % 10)
+//					compare = 1;
+//				else {
+//					temp1 /= 10;
+//					temp2 /= 10;
+//				}				
+//			}
+			
+//			while (temp1 > 0 && compare == 0) {
+//				if (temp1 % 10 > 2) {
+//					compare = -1;
+//				} else if (temp1 % 10 < 2) {
+//					compare = 1;
+//				} else {
+//					temp1 /= 10;
+//				}
+//			}
+//			
+//			while (temp2 > 0 && compare == 0) {
+//				if (temp2 % 10 > 2) {
+//					compare = 1;
+//				} else if (temp2 % 10 < 2) {
+//					compare = -1;
+//				} else {
+//					temp2 /= 10;
+//				}
+//			}
 			
 			if (compare == 0) {
 				compare = name.compareTo(other.name);
